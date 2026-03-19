9ea9f682741d3d742b4ebe10dba231c9d83c2e02
Lichi <lichimarquez04@gmail.com>
2026-03-19 15:05:10 +0000

    Exercise 1 finished

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -1,15 +1,40 @@
 import scala.io.Source
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
 object FileIO {
-  // Pure function to read subscriptions from a JSON file
-  def readSubscriptions(): List[String] = {
-    List(
-      "https://www.reddit.com/r/scala/.json?count=10",
-      "https://www.reddit.com/r/learnprogramming/.json?count=10"
-    )
+  /*
+    I asked ChatGPT why it is necessary, apparently json4s does not know how to convert JSON to scala types.
+    I do not even know why would you create a JSON library and not make to convert types in any way possible,
+      via function or something. This is unbealivable...
+  */
+  implicit val formats: DefaultFormats.type = DefaultFormats
+
+  //$ Define alias
+  type Subscription = (String, String)
+
+  //$ Pure function to read subscriptions from a JSON file
+  def readSubscriptions(): List[Subscription] = {
+    //& Read JSON File
+    val source = Source.fromFile("subscriptions.json")
+
+    //? Convert to String and close fd
+    val jsonString =
+      try source.mkString
+      finally source.close()
+
+    //+ Convert String to "JSON" format
+    val json = parse(jsonString)
+
+    //! Now we convert to List[Subscription] format
+    json.children.map { sub =>
+      val name = (sub \ "name").extract[String]
+      val url = (sub \ "url").extract[String]
+      (name, url)
+    }
   }
 
-  // Pure function to download JSON feed from a URL
+  //$ Pure function to download JSON feed from a URL
   def downloadFeed(url: String): String = {
     val source = Source.fromURL(url)
     source.mkString
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,18 +2,24 @@ object Main {
   def main(args: Array[String]): Unit = {
     val header = s"Reddit Post Parser\n${"=" * 40}"
 
-    val subscriptions: List[String] = FileIO.readSubscriptions()
+    //+ Exercise 1
+    val subscriptions = FileIO.readSubscriptions()
 
-    val allPosts: List[(String, String)] = subscriptions.map { url =>
-      println(s"Fetching posts from: $url")
-      val posts = FileIO.downloadFeed(url)
-      (url, posts)
-    }
+    println(subscriptions)
 
-    val output = allPosts
-      .map { case (url, posts) => Formatters.formatSubscription(url, posts) }
-      .mkString("\n")
+    //+ Exercise 2
 
-    println(output)
+
+    //+ Exercise 3
+
+
+    //+ Exercise 4
+
+
+    //+ Exercise 5
+
+
+    //+ Exercise 6
+    
   }
 }

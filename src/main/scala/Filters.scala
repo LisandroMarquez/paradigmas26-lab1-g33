0e928f146ee29c640a871d7b9c3050cf848a4dc2
Lichi <lichimarquez04@gmail.com>
2026-03-27 13:50:29 +0000

    File modularization for better reading and understanding

--- src/main/scala/Filters.scala
+++ src/main/scala/Filters.scala
@@ -1,11 +1,29 @@
 import FileIO.Post
 
 object Filters {
-  // Function to remove posts with empty title or selftext
-  def removeEmptyPosts(posts: List[Post]): List[Post] = {
+  //$ Individual filters for each case
+  def formatEmptySelftext(posts: List[Post]): List[Post] = {
     posts.filter {
-      case (_, title, selftext, _) =>
-        title.trim.nonEmpty && selftext.trim.nonEmpty
+      case =>
+        selftext.trim.nonEmpty
     }
   }
+
+  def formatEmptyTitle(posts: List[Post]): List[Post] = {
+    posts.filter {
+      case =>
+        title.trim.nonEmpty
+    }
+  }
+  /* Explanation for the future
+  *  [].trim removes every space, so we do not need an extra function to filter posts with only empty spaces
+  *  [].trim.nonEmpty returns a boolean:
+  *    true if contains text that is not only " " or ""
+  *    false if does not contains anything but " " or ""
+  */
+
+  //$ Function to filter post completely
+  def removeEmptyPosts(posts: List[Post]): List[Post] = {
+    val postsWithoutEmptySelftext = formatEmptySelftext(posts)
+    formatEmptyTitle(postsWithoutEmptySelftext)
 }

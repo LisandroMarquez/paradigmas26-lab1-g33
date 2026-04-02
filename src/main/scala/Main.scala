7a358fad735d4bb143261ff05da5ebf450e13030
Bautista Gonzalez <bautista.gonzalez.677@mi.unc.edu.ar>
2026-04-02 17:04:08 -0300

    Exercise 6: Making the report

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -24,43 +24,36 @@ object Main {
 
 
     //+ Exercise 6
-    println("PRUEBA DEL SCORE Y NAME DEL EJERCICIO 6")
+    println(s""" 
+=================================================================================================================================================
+                                                           NFORME SOBRE SUSCRIPCIONES
+-------------------------------------------------------------------------------------------------------------------------------------------------
+""")
     subscriptions match {
       case Some(subs) =>
         subs.foreach { sub =>
         val filter_posts = posts.filter(_._1 == sub._1)
         val score = FileIO.total_score(filter_posts)
-        println(s"Name: ${sub._1} Score: $score")
+        val frecuent_words = Analytics.topWords(filter_posts, 10) 
+        val first_5_post = FileIO.first_posts(filter_posts) 
+        println(s"""
+==================================================================== NOMBRE ====================================================================
+                                                                   ${sub._1}
+------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                  Score total: 
+                                                                    $score
+------------------------------------------------------------------------------------------------------------------------------------------------
+                                                        Top 10 Palabras con mayor frecuencia: 
+     $frecuent_words
+------------------------------------------------------------------------------------------------------------------------------------------------
+                                                       Primeros 5 post (Titulo, fecha, url):
+$first_5_post 
+================================================================================================================================================
+ """)
       }
 
       case None =>
-      println("Error leyendo suscripciones")
+      println("Error leyendo suscripciones") 
     } 
-
-    println("PRUEBA DE PALABRAS FRECUENTES CON SUS OCURRENCIAS")
-     subscriptions match {
-      case Some(subs) =>
-        subs.foreach { sub =>
-        val filter_posts = posts.filter(_._1 == sub._1)
-        val frecuent_words = Analytics.topWords(filter_posts, 10) 
-        println(s"Name: ${sub._1} Top 10 Word Frecuencies: $frecuent_words")
-      } 
-
-      case None => 
-      println("Error leyendo suscripciones")
-    } 
-
-    println("PRUEBA DE TOP 5 PALABRAS")
-      subscriptions match {
-      case Some(subs) =>
-        subs.foreach { sub =>
-        val filter_posts = posts.filter(_._1 == sub._1)
-        val first_5_post = FileIO.first_posts(filter_posts) 
-        println(s"Name: ${sub._1} First 5 posts: $first_5_post")
-      } 
-
-      case None =>
-      println("Error leyendo suscripciones")
-    }
   }
 }
